57a0c8c74cafb168d5a48e6938dd60f4bc7c5699
Christoforos Vradis <christoforos.vradis@onecentral.nl>
2023-12-04 16:14:51 +0100

    feat(functionatlity): add functionality and readme.md

---
+++ README.md
@@ -0,0 +1,76 @@
+# Tesla Range Calculator 🚗⚡
+
+Welcome to the Tesla Range Calculator - Frontend Assessment for Sendcloud! This application simulates the range of Tesla Model S variants (100D and P100D) under various conditions, including speed, temperature, air conditioning usage, and wheel size. It's built with modern web technologies to provide a responsive and interactive user experience.
+
+## Project Structure 📂
+
+tesla-range-calculator/
+│
+├── data/ - Contains JSON files with range data for different models.
+│ ├── metric-100D.json
+│ └── metric-P100D.json
+│
+├── public/ - Public assets like fonts and images.
+│ ├── fonts/
+│ └── images/
+│
+├── src/ - Source code for the application.
+│ ├── main.ts - Main TypeScript file with application logic.
+│ └── vite-env.d.ts - Type definitions for Vite.
+│
+├── styles/ - SCSS styles for the application.
+│ └── main.scss - Main stylesheet written in SCSS.
+│
+├── index.html - Entry point HTML file.
+├── package.json - Project metadata and dependencies.
+├── README.md - Documentation for the project.
+├── tsconfig.json - TypeScript configuration file.
+└── yarn.lock - Yarn lockfile for dependencies.
+
+## Features ✨
+
+- Calculate range based on different factors 📏
+- Interactive controls for simulation settings 🔧
+- Responsive design for various screen sizes (not really) 📱💻
+- Pure front-end implementation 🌐
+
+## Getting Started 🚀
+
+To run this project locally, follow these steps:
+
+1. **Clone the repository:**
+
+```bash
+git clone https://github.com/akuma-phawx/sendcloud-assessment
+cd sendcloud-assessment
+```
+
+2. **Install dependencies:**
+
+Make sure you have Yarn installed on your system.
+
+```bash
+yarn
+```
+
+3. **Start the development server:**
+
+Make sure you have Yarn installed on your system.
+
+```bash
+yarn dev
+```
+
+After running this command, the application should be available at http://localhost:5173 (or a different port if this one is busy).
+
+Open your browser and navigate to the application URL.
+
+## Usage 📋
+
+Adjust the settings for speed, temperature, wheel size, and AC to see how they affect the driving range of the Tesla Model S variants. The displayed range will update automatically based on your selections.
+
+## Contact 📬
+
+If you have any questions or want to reach out, drop me a line at chrisvradis00@gmail.com.
+
+Enjoy Marius! (And hey Andrei) 🎉
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,185 @@
+// Import styles
 import "../styles/main.scss";
-import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+// Define type for a single highway data point
+interface HighwayData {
+  kmh: number;
+  kilometers: number;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+// Define type for the range data
+interface RangeData {
+  temp: number;
+  wheelsize: number;
+  ac: string;
+  hwy: HighwayData[];
+}
+
+// Define type for the vehicle model data array
+type VehicleModelData = RangeData[];
+
+// Store the loaded data
+let data100D: VehicleModelData = [];
+let dataP100D: VehicleModelData = [];
+
+// Load data from JSON files
+function loadData(): void {
+  fetch("/data/metric-100D.json")
+    .then((response) => response.json())
+    .then((data: VehicleModelData) => {
+      data100D = data;
+    })
+    .catch((error) => console.error("Error loading 100D data:", error));
+
+  fetch("/data/metric-P100D.json")
+    .then((response) => response.json())
+    .then((data: VehicleModelData) => {
+      dataP100D = data;
+    })
+    .catch((error) => console.error("Error loading P100D data:", error));
+}
+
+// Function to get the AC status
+function getAcStatus(): string {
+  const acToggle = document.getElementById("acToggle") as HTMLInputElement;
+  return acToggle.checked ? "on" : "off";
+}
+
+// Function to find the range based on selected criteria
+function findRange(
+  data: VehicleModelData,
+  temp: number,
+  wheelSize: number,
+  acStatus: string,
+  speed: number
+): number | undefined {
+  const matchingData = data.find(
+    (d) => d.temp === temp && d.wheelsize === wheelSize && d.ac === acStatus
+  );
+  return matchingData?.hwy.find((s) => s.kmh === speed)?.kilometers;
+}
+
+// Function to update the range display
+function updateRangeDisplay(): void {
+  const temperatureInput = document.getElementById(
+    "temperature"
+  ) as HTMLInputElement;
+  const speedInput = document.getElementById("speed") as HTMLInputElement;
+  const selectedWheelSize =
+    document
+      .querySelector(".wheel-size.selected")
+      ?.textContent?.trim()
+      .replace('"', "") || "19";
+
+  const acStatus = getAcStatus();
+  const temperature = parseInt(temperatureInput.value, 10);
+  const wheelSize = parseInt(selectedWheelSize, 10);
+  const speed = parseInt(speedInput.value, 10);
+
+  // Calculate range for 100D
+  const range100D = findRange(
+    data100D,
+    temperature,
+    wheelSize,
+    acStatus,
+    speed
+  );
+  if (range100D !== undefined) {
+    const rangeDisplay100D = document.getElementById("100d-kms") as HTMLElement;
+    rangeDisplay100D.textContent = `${range100D}`;
+  }
+
+  // Calculate range for P100D
+  const rangeP100D = findRange(
+    dataP100D,
+    temperature,
+    wheelSize,
+    acStatus,
+    speed
+  );
+  if (rangeP100D !== undefined) {
+    const rangeDisplayP100D = document.getElementById(
+      "p100d-kms"
+    ) as HTMLElement;
+    rangeDisplayP100D.textContent = `${rangeP100D}`;
+  }
+}
+
+// DOMContentLoaded event listener
+document.addEventListener("DOMContentLoaded", () => {
+  // Load data initially
+  loadData();
+
+  // Update the range display after a short delay to ensure data has loaded
+  setTimeout(updateRangeDisplay, 1000); // Adjust this delay based on your fetch performance
+
+  // Event listeners for speed and temperature controls
+  const buttonUpSpeed = document.querySelector(
+    ".button-up-speed"
+  ) as HTMLButtonElement;
+  const buttonDownSpeed = document.querySelector(
+    ".button-down-speed"
+  ) as HTMLButtonElement;
+  const buttonUpTemperature = document.querySelector(
+    ".button-up-temperature"
+  ) as HTMLButtonElement;
+  const buttonDownTemperature = document.querySelector(
+    ".button-down-temperature"
+  ) as HTMLButtonElement;
+  const wheels = document.querySelectorAll(".wheel-size");
+  const acToggle = document.getElementById("acToggle") as HTMLInputElement;
+
+  // Add event listeners to all control buttons
+  buttonUpSpeed.addEventListener("click", () => {
+    updateSpeed(10);
+    updateRangeDisplay();
+  });
+  buttonDownSpeed.addEventListener("click", () => {
+    updateSpeed(-10);
+    updateRangeDisplay();
+  });
+  buttonUpTemperature.addEventListener("click", () => {
+    updateTemperature(10);
+    updateRangeDisplay();
+  });
+  buttonDownTemperature.addEventListener("click", () => {
+    updateTemperature(-10);
+    updateRangeDisplay();
+  });
+  // Event listener for AC toggle
+  acToggle.addEventListener("change", () => {
+    updateRangeDisplay(); // Call updateRangeDisplay when AC state changes
+  });
+  // Add click event listeners to wheel size buttons
+  wheels.forEach((wheel) => {
+    wheel.addEventListener("click", function (this: HTMLElement) {
+      // Remove 'selected' class from all wheels
+      wheels.forEach((w) => w.classList.remove("selected"));
+      // Add 'selected' class to the clicked wheel
+      this.classList.add("selected");
+      updateRangeDisplay();
+    });
+  });
+
+  // Update Speed Function
+  function updateSpeed(change: number): void {
+    const speedInput = document.getElementById("speed") as HTMLInputElement;
+    let currentSpeed = parseInt(speedInput.value, 10);
+    let newSpeed = currentSpeed + change;
+    newSpeed = Math.max(70, newSpeed); // Minimum speed
+    newSpeed = Math.min(140, newSpeed); // Maximum speed
+    speedInput.value = newSpeed.toString();
+  }
+
+  // Update Temperature Function
+  function updateTemperature(change: number): void {
+    const temperatureInput = document.getElementById(
+      "temperature"
+    ) as HTMLInputElement;
+    let currentTemperature = parseInt(temperatureInput.value, 10);
+    let newTemperature = currentTemperature + change;
+    newTemperature = Math.max(-10, newTemperature); // Minimum temperature
+    newTemperature = Math.min(40, newTemperature); // Maximum temperature
+    temperatureInput.value = newTemperature.toString();
+  }
+});
